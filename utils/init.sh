a0f87dfd17467561ec509cdb70e6dfdcbd6e118c
mbay <murat.bayram@softwareag.com>
2021-10-15 15:25:24 +0200

    initial commit

---
+++ utils/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This file is part of DRK Impfterminerfassung.
+
+cp /home/webservice/Backup/logo_impfzentrum.png /home/webservice/Antikoerpererfassung/utils/logo.png
+cp /home/webservice/Backup/config_antikoerper.ini /home/webservice/Antikoerpererfassung/config.ini
