be2e08971f0ccc7585a896be57202ce82435dfd3
mbay <murat.bayram@softwareag.com>
2021-11-05 12:17:58 +0100

    creation of export and reporting

---
+++ TagesReport/job.py
@@ -0,0 +1,46 @@
+import logging
+import locale
+import time
+import datetime
+import sys
+import csv
+import numpy as np 
+from pdfcreator.pdf import PDFgenerator
+sys.path.append("..")
+from utils.database import Database
+from utils.sendmail import send_mail_report
+from utils.getRequesterMail import get_Leitung_from_StationID
+
+
+logFile = '../../Logs/Antikoerper/TagesreportJob.log'
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('Tagesreport')
+logger.info('Starting Tagesreporting')
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv)  == 2:
+            requestedDate = sys.argv[1]
+            send=False
+        elif len(sys.argv) == 3:
+            requestedDate = sys.argv[1]
+            send=True
+        else:
+            logger.debug('Input parameters are not correct, date and/or requested needed')
+            raise Exception
+        DatabaseConnect = Database()
+        sql = "Select count(Voranmeldung.id), Impfstoff.Kurzbezeichnung from Voranmeldung LEFT JOIN Termine ON Termine.id=Voranmeldung.Termin_id LEFT JOIN Station ON Station.id=Termine.id_station LEFT JOIN Impfstoff ON Impfstoff.id=Station.Impfstoff_id GROUP BY Impfstoff.id where Voranmeldung.Used=1 and Voranmeldung.Tag Between '%s 00:00:00' and '%s 23:59:59';" % (requestedDate.replace('-', '.'), requestedDate.replace('-', '.'))
+        logger.debug('Getting all Events for a date with the following query: %s' % (sql))
+        exportEvents = DatabaseConnect.read_all(sql)
+        logger.debug('Received the all entries: %s' %(str(exportEvents)))
+        PDF = PDFgenerator(exportEvents, f"{requestedDate}")
+        filename = PDF.generate()
+        if send:
+            logger.debug('Sending Mail')
+            send_mail_report(filename,requestedDate,get_Leitung_from_StationID(0))
+        logger.info('Done')
+    except Exception as e:
+        logging.error("The following error occured: %s" % (e))
+    finally:
+        DatabaseConnect.close_connection()
