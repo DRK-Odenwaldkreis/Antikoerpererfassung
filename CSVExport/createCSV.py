9b8fee3c23e4eec859c048a8fb919638d68b3492
mbay <murat.bayram@softwareag.com>
2021-11-30 13:51:01 +0100

    adding export feature

---
+++ CSVExport/createCSV.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of DRK Impfzentrum.
+
+
+import sys
+import csv
+import logging
+from utils.slot import get_slot_time
+
+from datetime import datetime
+sys.path.append("..")
+
+
+logger = logging.getLogger('CSV Export')
+logger.debug('Logger for createCSV was initialised')
+
+
+def create_CSV(content, date):
+    filename = "../../Reports/Antikoerper/export_" + str(date) + ".csv"
+    with open(filename, mode='w', newline='') as csvfile:
+        writeEntry = csv.writer(csvfile, delimiter=';')
+        writeEntry.writerow(["id",
+                             "Nachname",
+                             "Vorname",
+                             "Telefonnummer",
+                             "Mailadresse",
+                             "Datum",
+                             "Uhrzeit"
+                             ])
+        for i in content:
+            j = list(i)
+            slot = j[8]
+            stunde = j[7]
+            j.pop()
+            j.pop()
+            j.append(get_slot_time(slot,stunde))
+            writeEntry.writerow(j)
+    return filename
+
